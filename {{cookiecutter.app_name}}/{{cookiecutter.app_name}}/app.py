a744246e0165d48816c4cc37ea813ac0d7079d4b
marcsello <punkosdmarcell@rocketmail.com>
2020-03-29 01:53:51 +0100

    Fixed #1

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/app.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/app.py
@@ -18,29 +18,24 @@ from api_views import HelloView
 # create flask app
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1)
-
 # configure flask app
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', "sqlite://")  # Default to memory db
+{% if cookiecutter.use_sqlalchemy == 'y' %}app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', "sqlite://")  # Default to memory db
+{% endif %}
 {% if cookiecutter.generate_api_view == 'y' %}
 app.config['LOCAL_API_KEY'] = os.environ['LOCAL_API_KEY']
 {% endif %}
-
 # important stuff
 app.secret_key = os.environ.get('SECRET_KEY', os.urandom(12))
-
-# disable this for better performance
+{% if cookiecutter.use_sqlalchemy == 'y' %}# disable this for better performance
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-# initialize stuff
+{% endif %}
 {% if cookiecutter.use_sqlalchemy == 'y' %}
+# initialize stuff
 db.init_app(app)
 
 with app.app_context():
 	db.create_all()
 {% endif %}
-
-
 # register error handlers
 register_all_error_handlers(app)
 
@@ -53,8 +48,6 @@ for view in [HomeView]:
 for view in [HelloView]:
 	view.register(app, trailing_slash=False, route_prefix="/api/")
 {% endif %}
-
-
 # start debuggig if needed
 if __name__ == "__main__":
 	app.run(debug=True)
